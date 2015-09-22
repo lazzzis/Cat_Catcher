931b07b5b2bb78577ec473774c61aea57ba9c976
lazzzis <shenlijin@outlook.com>
2015-09-22 16:00:56 +0800

    add basic files

---
+++ dl.py
@@ -0,0 +1,44 @@
+__author__ = 'lazzzis'
+
+import urllib.request
+import sys
+import re
+
+
+def get_url_html(page):
+    req = urllib.request.Request(page)
+    req.add_header("User-Agent", "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0")
+    response = urllib.request.urlopen(req)
+    return response.read()
+
+
+def save_pic(filename, info):
+    with open(filename, "wb") as f:
+        f.write(info)
+
+
+def get_pic_url(html):
+    """This function is temporary useless
+    """
+    p = re.compile('src="([^"]*)')
+    return p.findall(p.findall(html))
+
+
+"""3 arguments are needed
+The first one and second one are the width and height of the picture respectively
+The third one is the filename and this is optional. If the second one is not provided, the pic will save on the current
+folder
+"""
+if __name__ == '__main__':
+    try:
+        width, height = sys.argv[1], sys.argv[2]
+        html = get_url_html("http://placekitten.com/" + width + "/" + height)
+        if html == b'':
+            print("No such picture with the given size " + width + "*" + height)
+        else:
+            filename = width + '-' + height + '.jpg' if len(sys.argv) < 4 else sys.argv[3]
+            save_pic(filename, html)
+            print("The picture is downloaded successfully!")
+
+    except IndexError:
+        print("The program can't work because it need at lease two argumentss!")
