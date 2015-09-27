01646dfb2228accef337e07f7c978f17df7f71e6
lazzzis <shenlijin@outlook.com>
2015-09-27 21:35:22 +0800

    add a title for the simple window

--- ui.py
+++ ui.py
@@ -12,15 +12,14 @@ def center_window(root, width, height):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-
     # calculate position x and y coordinates
     x = (screen_width/2) - (width/2)
     y = (screen_height/2) - (height/2)
     root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 
-
 master = tk.Tk()
+master.title('cat_catcher')
 
 tk.Label(master, text="width").grid(row=0, column=0)
 tk.Label(master, text="length").grid(row=1, column=0)
